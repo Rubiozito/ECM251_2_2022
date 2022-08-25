27c4e5d8ba2da7f24528a3c812b9579b675c0395
Rubiozito <rafaelrubiocarnes@gmail.com>
2022-08-25 10:58:08 -0300

    Tratando exceções

--- Aula 18/main.py
+++ Aula 18/main.py
@@ -1,11 +1,22 @@
 #Código para escrever dados em um arquivo
 
-arquivo = open("data/sistemas.txt", "w")
-continuar = True
-while continuar:
-    os_name = input('Diga um OA (vazio para sair):')
-    if not os_name:
-        continuar = False
-        continue
-    arquivo.write(os_name)
-arquivo.close
+from traceback import print_stack
+
+
+try:
+    arquivo = open("data/sistemas.txt", "w")
+    continuar = True
+    while continuar:
+        os_name = input('Diga um OA (vazio para sair):')
+        if not os_name:
+            continuar = False
+            continue
+        arquivo.write(os_name)
+    arquivo.close
+except FileNotFoundError:
+    print('Caminho não existe, favor verificar')
+except Exception:
+    print("Algo não esperado ocorreu:")
+    print_stack()
+finally:
+    print("Chegamos no final!")
