34988e9acdc863b4baa5b57cf9b6aca2c5e4d829
Rubiozito <rafaelrubiocarnes@gmail.com>
2022-09-28 17:45:27 -0300

    criação da pasta do t3

--- Aula17/modelos/pagamentos/boleto.py
+++
@@ -1,8 +0,0 @@
-from datetime import date
-class Boleto(Pagamento):
-    def __init__(self, valor, dias_para_vencimento = 0):
-        self._valor = valor
-        self._vencimento = date.today() + dias_para_vencimento
-    def realizar_pagamento(self):
-        return True
-    
