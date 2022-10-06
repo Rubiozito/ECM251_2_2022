3926ae3f6821ee7ac24dda01f989d213eecfb2b8
Rubiozito <rafaelrubiocarnes@gmail.com>
2022-10-06 11:05:13 -0300

    .

--- Aula 21/src/view/lista_de_tarefas_view.py
+++ Aula 21/src/view/lista_de_tarefas_view.py
@@ -2,5 +2,7 @@ import streamlit as st
 from controllers.tarefa_controller import TarefaController
 
 class ListaTarefasView:
-    def __init__(self) -> None:
-        
+    def __init__(self, controller) -> None:
+        self.controller = controller 
+        self.descricao_tarefa = st.text_input
+
