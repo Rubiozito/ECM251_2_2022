a85ee9dfae6a31cb2acfb1bd2828490b8ae3bd25
Rubiozito <rafaelrubiocarnes@gmail.com>
2022-10-09 19:13:50 -0300

    login user

--- AtividadeT3/src/controllers/user_controller.py
+++ AtividadeT3/src/controllers/user_controller.py
@@ -3,3 +3,5 @@ from models.user import User
 class UserController():
     def __init__(self) -> None:
         self.user = []
+
+    
--- AtividadeT3/src/models/user.py
+++ AtividadeT3/src/models/user.py
@@ -1,11 +1,26 @@
 class User():
 
     def __init__(self, name, email, password ):
-        self.name = name
-        self.email = email
-        self.password = password
+        self._name = name
+        self._email = email
+        self._password = password
+
+    def login(self, email_login, password_login):
+        if email_login == self._email & password_login == self._password:
+            return True
+        else:
+            return False
 
     def __str__(self) -> str:
         return f'User (name:{self.name}, email:{self.email}, password:{self.password})'
 
-    def get_name
+    def get_name(self):
+        return self._name
+
+    def get_password(self):
+        return self._password
+
+    def get_email(self):
+        return self._email
+
+    
