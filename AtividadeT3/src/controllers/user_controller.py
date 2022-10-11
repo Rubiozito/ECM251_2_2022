99965e32c6a5d068eead86a38e3669cc62ffae7e
Rubiozito <rafaelrubiocarnes@gmail.com>
2022-10-11 09:19:18 -0300

    .

--- AtividadeT3/src/controllers/user_controller.py
+++ AtividadeT3/src/controllers/user_controller.py
@@ -1,7 +1,9 @@
+import streamlit as st
 from models.user import User
 
 class UserController():
-    def __init__(self) -> None:
-        self.user = []
+    def __init__(self):
+        if "user" not in st.session_state:
+            st.session_state.user = None
 
     
