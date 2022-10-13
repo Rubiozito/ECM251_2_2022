7f4fa04678d581d72bc58635f2aa8d68ff6253c7
Rubiozito <rafaelrubiocarnes@gmail.com>
2022-10-12 23:48:50 -0300

    home e produtos

--- AtividadeT3/src/views/home_page.py
+++ AtividadeT3/src/views/home_page.py
@@ -1,4 +1,21 @@
 import streamlit as st
 
 def show_home_page():
-   home, produtos, carrinho = st.tabs(['Home', 'Produtos', 'Carrinho'])
+   main, produtos, carrinho = st.tabs(['Home', 'Produtos', 'Carrinho'])
+
+   with main:
+      st.title("Bem vindo a lohjinha da Mel")
+      st.image('assets/mel_1.jpeg')
+      st.write("Aqui você poderá encontrar varias fotos da salsicha mais carismática do Brasil")
+      
+
+   with produtos:
+      col1, col2, col3 = st.columns(3)
+      
+      with col1:
+         st.image('assets/Mel_HotDog.jpeg')
+         st.write('Mel com skin de Hot-Dog')
+         st.write('R$10.000')
+         st.button(label='Adicionar ao carrinho', help = 'clique para adicionar')
+
+      
