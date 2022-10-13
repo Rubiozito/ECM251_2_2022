68318bc525c20bf995894b6b50c3011a3bc9d0db
Rubiozito <rafaelrubiocarnes@gmail.com>
2022-10-13 00:05:16 -0300

    produtos feito(sem carrinho)

--- AtividadeT3/src/views/home_page.py
+++ AtividadeT3/src/views/home_page.py
@@ -6,7 +6,7 @@ def show_home_page():
    with main:
       st.title("Bem vindo a lohjinha da Mel")
       st.image('assets/mel_1.jpeg')
-      st.write("Aqui você poderá encontrar varias fotos da salsicha mais carismática do Brasil")
+      st.subheader("Aqui você poderá encontrar varias fotos da salsicha mais carismática do Brasil")
       
 
    with produtos:
@@ -15,7 +15,22 @@ def show_home_page():
       with col1:
          st.image('assets/Mel_HotDog.jpeg')
          st.write('Mel com skin de Hot-Dog')
-         st.write('R$10.000')
-         st.button(label='Adicionar ao carrinho', help = 'clique para adicionar')
+         st.subheader('R$10.000')
+         st.button(label='Adicionar ao carrinho', help = 'clique para adicionar', key = 1)
 
+      with col2:
+         st.image('assets/Mel_com_capa_de_chuva.jpeg')
+         st.write('Mel com skin de capa de chuva')
+         st.subheader('R$ 4.000')
+         st.button(label='Adicionar ao carrinho', help = 'clique para adicionar', key = 2)
+
+
+      with col3:
+         st.image('assets/Mel_Cone.jpeg')
+         st.write('Mel com skin de satélite')
+         st.subheader('R$5.000')
+         st.button(label='Adicionar ao carrinho', help = 'clique para adicionar', key = 3)
+
+   with carrinho:
+      st.error("Este carrinho foi perdido enquanto procurava a nota do T2")
       
