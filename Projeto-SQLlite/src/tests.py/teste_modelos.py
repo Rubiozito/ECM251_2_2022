809147ee7d3bada62bc0155147fc19f57771c342
Rubiozito <rafaelrubiocarnes@gmail.com>
2022-10-19 10:01:30 -0300

    Aula 19-10

---
+++ Projeto-SQLlite/src/tests.py/teste_modelos.py
@@ -0,0 +1,5 @@
+from src.models.pedido import Pedido
+from src.models.item import Item
+
+pedido1 = Pedido(1, 'ABC', 10, 'qwerty', 'teste', '19/10/2022-09:18')
+pedido2 = Pedido(2, 'SQL', 2, 'qwerty', 'teste', '19/10/2022-09:20')
