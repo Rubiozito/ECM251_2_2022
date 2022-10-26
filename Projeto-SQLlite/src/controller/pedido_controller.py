72a470f885a4d6d517d35940f2fd99ae45564abc
Rubiozito <rafaelrubiocarnes@gmail.com>
2022-10-26 13:46:15 -0300

    atualização final da aula

--- Projeto-SQLlite/src/controller/pedido_controller.py
+++ Projeto-SQLlite/src/controller/pedido_controller.py
@@ -4,18 +4,20 @@ from src.controllers.item_controller import ItemController
 class PedidoController:
     def __init__(self) -> None:
         pass
-
     def total_pedido(self, numero_pedido) -> float:
-        items_pedido = PedidoDAO.get_instance().get_itens(numero_pedido)
+        items_pedido = PedidoDAO.get_instance().pegar_pedido(numero_pedido)
         total = 0
         item_controller = ItemController()
-        for (item_id, quantidade) in items_pedido:
-            item = item_controller.pegar_item(item_id)
-            total += item.preco * quantidade
+        for item in items_pedido:
+            item_elemento = item_controller.pegar_item(item.id_item)
+            total += item_elemento.preco * item.quantidade
         return total
-
     def pegar_pedido(self, numero_pedido)-> list[Pedido]:
         return PedidoDAO.get_instance().pegar_pedido(numero_pedido)
 
     def atualizar_pedido(self, pedido)-> bool:
         return PedidoDAO.get_instance().atualizar_pedido(pedido)
+    def deletar_pedido(self, id) ->bool:
+        return PedidoDAO.get_instance().deletar_item(id)
+    def inserir_pedido(self, pedido)->None:
+        PedidoDAO.get_instance().inserir_pedido(pedido)
