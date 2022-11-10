b88f28577f50a96bd9fa0b919049d264dca8bc2b
Rubiozito <rafaelrubiocarnes@gmail.com>
2022-11-09 22:33:49 -0300

    classe user

---
+++ Atividade T4/src/models/user.py
@@ -0,0 +1,24 @@
+class User():
+    
+    def __init__(self, name, email, password):
+        self.name =  name
+        self.email = email
+        self.password = password
+         
+    def __str__(self):
+        return f"User: {self.name}, {self.email}"
+    
+    def get_password(self):
+        return self.password
+    
+    def set_password(self, password):
+        self.password = password
+
+    def get_name(self):
+        return self.name
+    
+    def get_email(self):
+        return self.email
+
+    def set_email(self, email):
+        self.email = email
