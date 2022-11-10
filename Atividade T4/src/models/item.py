1d7ec2cbf99364d7d3c3baf6e4cf77227b056bea
Rubiozito <rafaelrubiocarnes@gmail.com>
2022-11-10 01:33:34 -0300

    classe item(e streamlit)

---
+++ Atividade T4/src/models/item.py
@@ -0,0 +1,10 @@
+class Item():
+
+    def __init__(self, id, name, price, description):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.description = description
+        
+
+    
