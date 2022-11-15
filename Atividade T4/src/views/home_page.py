e2c4da74c155f6238e863692300fe51520e0d393
Rubiozito <rafaelrubiocarnes@gmail.com>
2022-11-15 00:32:45 -0300

    .

--- Atividade T4/src/views/home_page.py
+++ Atividade T4/src/views/home_page.py
@@ -2,4 +2,15 @@ import streamlit as st
 
 
 def home_page():
-    st.header("Hello World")
+    home, produtos, carrinho = st.tabs(["Home", "Produtos", "Carrinho"])
+    
+
+    with home:
+        st.title("Bem vindo a lohjinha da Mel")
+        st.image('assets/mel_1.jpeg')
+        st.subheader("Aqui você poderá encontrar varias fotos da salsicha mais carismática do Brasil")
+
+
+    with produtos:
+        st.title("Produtos")
+         col1, col2, col3 = st.columns(3)
